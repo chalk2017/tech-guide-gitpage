abd551d65fe3b28e4c0f06c0d3a52f00f168c2bb
home <chaoquny@cn.ibm.com>
2022-01-07 11:34:22 +0800

    first commit

---
+++ precache-manifest.4f65f7474b0121053ec4dc08ea9331f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b30a0ddc47d17dc319f",
+    "url": "/css/app.7ee60460.css"
+  },
+  {
+    "revision": "0b800b6ba7e7554e7ee0cdf6477da514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b30a0ddc47d17dc319f",
+    "url": "/js/app.c3b8c10c.js"
+  },
+  {
+    "revision": "a95458e8b5c55cacc628",
+    "url": "/js/chunk-2d0c034d.83172644.js"
+  },
+  {
+    "revision": "ceb09f1da9a0afde8fef",
+    "url": "/js/chunk-61d1706e.9212f334.js"
+  },
+  {
+    "revision": "c4780f2c139e75df6790",
+    "url": "/js/chunk-vendors.a57ee9c7.js"
+  },
+  {
+    "revision": "f3cb0ac398a8afd8c81efdc64788f24f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
