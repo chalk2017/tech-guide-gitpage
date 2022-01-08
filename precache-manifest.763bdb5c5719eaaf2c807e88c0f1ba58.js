a2b2626d28c2c8b9aaa3747c46e5b69eb15e3dcf
home <chaoquny@cn.ibm.com>
2022-01-08 16:59:45 +0800

    'update'

---
+++ precache-manifest.763bdb5c5719eaaf2c807e88c0f1ba58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c4fc1036ee7a5794b3",
+    "url": "/tech-guide-gitpage/css/app.7ee60460.css"
+  },
+  {
+    "revision": "d6005f6129fd74335103c271abb790ee",
+    "url": "/tech-guide-gitpage/index.html"
+  },
+  {
+    "revision": "41c4fc1036ee7a5794b3",
+    "url": "/tech-guide-gitpage/js/app.1239110b.js"
+  },
+  {
+    "revision": "6d02035ad32f5fb1a6e6",
+    "url": "/tech-guide-gitpage/js/chunk-2d0c034d.7b2cc579.js"
+  },
+  {
+    "revision": "1e28e4db0aedf77d9230",
+    "url": "/tech-guide-gitpage/js/chunk-2d208839.ad4aebc9.js"
+  },
+  {
+    "revision": "df5a129179719f9c1f90",
+    "url": "/tech-guide-gitpage/js/chunk-6fd81fa5.a7fd4860.js"
+  },
+  {
+    "revision": "d8a90dcd607d2ede8066",
+    "url": "/tech-guide-gitpage/js/chunk-vendors.62469edc.js"
+  },
+  {
+    "revision": "f3cb0ac398a8afd8c81efdc64788f24f",
+    "url": "/tech-guide-gitpage/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tech-guide-gitpage/robots.txt"
+  }
+]);
